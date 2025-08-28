8f4e122cb1746324a2449a89de5c39c76905769e
Edarka <edara@vk.com>
2025-08-28 21:09:38 +0300

    Delete Main directory

--- Main/JS/Burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('burger');
-    const nav = document.querySelector('.nav');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('open');
-        burger.classList.toggle('active'); // можно анимировать иконку
-    });
-});
